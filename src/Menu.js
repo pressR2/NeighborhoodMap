e64bb0d95259fb96cb6633eb3e2f38f0b8a29fb5
pressR2 <36776929+pressR2@users.noreply.github.com>
2018-08-01 20:53:51 +0200

    feat: add location names to menu

--- src/Menu.js
+++ src/Menu.js
@@ -1,21 +1,29 @@
 import React from "react";
+import * as data from "./locations.json";
 
 
 class Menu extends React.Component {
+    state = {
+        locations: data,
+        list: []
+    };
+
     render() {
+        for (var i = 0; i < this.state.locations.length; i++) {
+            var title = this.state.locations[i].name;
+            this.state.list.push(<li>{title}</li>);
+        }
+
+
         return (
             <div id ="menu">        
                 
-                <h3 className="list-title">Burgers bar</h3>
-                <input className="input-width" type="text" placeholder="Bar location" />
+                <h3 className="list-title">Must see in Wrocław</h3>
+                <input className="input-width" type="text" placeholder="Filter location" />
                 <button className="filter">Filter</button>
                 <ol className="burgers-list">
-                
-                  <li className="burger">Eat Me</li>
-                  <li className="burger">Awasome burger</li>
-                  <li className="burger">Burger the best</li>
-                  <li className="burger">Vege burger</li>
-                  <li className="burger">Muuu</li>
+
+                    {this.state.list}
                 
                 </ol>            
         </div>
