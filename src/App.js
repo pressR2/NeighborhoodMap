e527023ddd768499ca46b5327095d512f84628eb
pressR2 <36776929+pressR2@users.noreply.github.com>
2018-08-01 20:00:12 +0200

    feat: add markers to the map

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import Hamburger from './Hamburger';
 import GoogleMaps from "./GoogleMaps";
 import Menu from './Menu'
 
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+   
+
+
+
   render() {
     return (
       <div className="App">
--- src/GoogleMaps.js
+++ src/GoogleMaps.js
@@ -1,35 +1,65 @@
-import React  from 'react';
+import React from "react";
+import * as data from "./locations.json";
 
 class GoogleMaps extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.initMap = this.initMap.bind(this);
+    }
+
+    state = {
+        markers: [],
+        locations: data,
+        map: undefined
+    };
+
+    initMap() {
+        this.state.map = new window.google.maps.Map(document.getElementById("map"), {
+            center: { lat: 51.107885, lng: 17.038538 },
+            zoom: 13
+        });
+
+        for (var i = 0; i < this.state.locations.length; i++) {
+            var position = this.state.locations[i].location;
+            var title = this.state.locations[i].name;
+
+            var marker = new window.google.maps.Marker({
+                position: position,
+                map: this.state.map,
+                title: title,
+                animation: window.google.maps.Animation.DROP,
+                id: i
+            });
+
+            this.state.markers.push(marker);
+            marker.addListener("click", function() {
+                console.log("marker klikniety");
+                /*populateInfoWindow(this, largeInfoWindow); nie zdefiniowana jeszcze*/
+            });
+        }
+
+        let markerInfo = marker => {
+            var markerTitle = marker.title;
+            var url = "";
+        };
+    }
 
     componentDidMount() {
-        window.initMap = initMap
-        loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyADMO5nZRIo1qyxLxlV1UAeTtEtr6osOuw&callback=initMap')
+        window.initMap = this.initMap;
+        loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyADMO5nZRIo1qyxLxlV1UAeTtEtr6osOuw&callback=initMap");
     }
 
     render() {
-      
-        return (
-            <div id='map'></div>
-        )   
+        return <div id="map" />;
     }
 }
 
 const loadScript = function(src) {
-  var tag = document.createElement('script');
-  tag.async = true;
-  tag.src = src;
-  document.getElementsByTagName('body')[0].appendChild(tag);
-}
+    var tag = document.createElement("script");
+    tag.async = true;
+    tag.src = src;
+    document.getElementsByTagName("body")[0].appendChild(tag);
+};
 
-const initMap = function() {
-
-
-    let ourMap = new window.google.maps.Map(document.getElementById('map'), {
-        center: {lat: 51.126858, lng: 16.986331},
-        zoom:15
-    })
-}
-
-export default GoogleMaps
-
+export default GoogleMaps;
